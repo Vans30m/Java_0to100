c8ada49b3fc611e375ec38af65b016c53f84d682
Vansh Thakur <vthakur.290905@gmail.com>
2025-07-10 18:41:26 +0530

    changes in nested switch statements
    Still not working

--- src/Lectures/Lecture_3/Nested_Enhanced_Switch.java
+++ src/Lectures/Lecture_3/Nested_Enhanced_Switch.java
@@ -4,7 +4,19 @@ package Lectures.Lecture_3;
 import java.util.Scanner;
 
 public class Nested_Enhanced_Switch {
-    public static void msinn(String[] args){
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter ID: ");
+        int id = in.nextInt();
+        in.nextLine(); // consume leftover newline
+
+        String dep = ""; // declare outside for access below
+
+        if(id == 3) {
+            System.out.print("Enter Department (IT or Finance): ");
+            dep = in.nextLine();
+        }
+
         switch (id) {
         case 1 -> System.out.println("Vansh Thakur");
         case 2 -> System.out.println("Tanishq Thakur");
--- src/Lectures/Lecture_3/Nested_Switch.java
+++ src/Lectures/Lecture_3/Nested_Switch.java
@@ -5,7 +5,7 @@ package Lectures.Lecture_3;
 import java.util.Scanner;
 
 public class Nested_Switch {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Input employee ID : ");
@@ -14,26 +14,27 @@ public class Nested_Switch {
         System.out.print("Input the department : ");
         String dep = in.next();
 
-        switch (id){
-            case 1 :
+        switch (id) {
+            case 1:
                 System.out.println("Vansh Thakur");
-                break;
-            case 2 :
+                continue ;
+            case 2:
                 System.out.println("Tanishq Thakur");
-                break;
-            case 3 :
-                switch (dep){
-                    case "IT" :
+//                break;
+            case 3:
+                switch (dep) {
+                    case "IT":
                         System.out.println("IT department");
                         break;
-                    case "Finance" :
+                    case "Finance":
                         System.out.println("Finance department");
                         break;
-                    default :
+                    default:
                         System.out.println("Incorrect Department");
                 }
                 break;
-            default :
+            default:
                 System.out.println("Incorrect ID");
         }
+    }
 }
